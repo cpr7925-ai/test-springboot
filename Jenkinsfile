7c3da8162d4ad96e0ab6b63900585adeeb1922e0
Naomi <cpr7925@gmail.com>
2025-12-23 09:56:09 +0800

    test failed

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
 
         stage('Build') {
             steps {
-                bat 'mvn clean package -DskipTests'
+                bat 'mvn clean package'
             }
         }
 
--- src/test/java/com/megabank/exam/ExamApplicationTests.java
+++ src/test/java/com/megabank/exam/ExamApplicationTests.java
@@ -3,11 +3,14 @@ package com.megabank.exam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class ExamApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("故意讓測試失敗");
 	}
 
 }
